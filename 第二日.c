3def43539ad84bbc62bbfb1a691ab286b008937d
weijifenlove <weijifenlove@gmail.com>
2017-07-18 09:40:43 +0800

    second day

---
+++ 第二日.c
@@ -0,0 +1,14 @@
+//第二天鞥能量输出
+# include<stdio.h>
+int main()
+
+{
+    int i,j;
+    for(i=0,j=0;i++;i<100)
+    {
+        j=j+i;
+        printf("the j number is %d\n",j);
+
+    }
+    return 0;
+}
